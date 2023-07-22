7eee497a4881ab820b19786a64ef6b83c3573e2a
Marco Rensch <marco.rensch@nx-designs.ch>
2023-07-22 09:13:00 +0200

    Added Leagues Table

--- administrator/components/com_footballmanager/sql/install.mysql.utf8.sql
+++ administrator/components/com_footballmanager/sql/install.mysql.utf8.sql
@@ -71,4 +71,31 @@ CREATE TABLE IF NOT EXISTS `#__footballmanager_sponsors`
     CONSTRAINT `fk_sponsors_modified_by` FOREIGN KEY (`modified_by`) REFERENCES `#__users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS `#__footballmanager_leagues`
+(
+    `id`           int(11)          NOT NULL AUTO_INCREMENT,
+    `title`        varchar(255)     NOT NULL,
+    `alias`        varchar(255)     NOT NULL,
+    `image`        varchar(255)              DEFAULT NULL,
+    `pts_loose`    int(11)                   DEFAULT 0,
+    `pts_tie`      int(11)                   DEFAULT 0,
+    `pts_win`      int(11)                   DEFAULT 0,
+    `params`       text,
+    `state`        tinyint(3)       NOT NULL DEFAULT 0,
+    `published`    tinyint(1)       NOT NULL DEFAULT 0,
+    `created_at`   datetime                  DEFAULT NULL,
+    `created_by`   int(11)                   DEFAULT NULL,
+    `modified_at`  datetime                  DEFAULT NOW(),
+    `modified_by`  int(11)                   DEFAULT NULL,
+    `version`      int(11)                   DEFAULT 0,
+    `hits`         int(11)                   DEFAULT 0,
+    `access`       int(10) unsigned NOT NULL DEFAULT 0,
+    `ordering`     int(11)                   DEFAULT 0,
+
+    PRIMARY KEY (`id`),
+    CONSTRAINT `unique_columns` UNIQUE (`alias`),
+    CONSTRAINT `fk_leagues_created_by` FOREIGN KEY (`created_by`) REFERENCES `#__users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT `fk_leagues_modified_by` FOREIGN KEY (`modified_by`) REFERENCES `#__users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
 /* Auto Generate: GENERATED ALWAYS AS (lower(replace(name, ' ', '-'))) STORED, */
