cb20fcab0decf0278e97ea92693f4e266bd8a0a7
Marco Rensch <marco.rensch@nx-designs.ch>
2023-12-15 15:40:43 +0100

    Pro Features Teams

---
+++ components/com_footballmanager/tmpl/teams/default.item.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_footballmanager
+ *
+ * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use Joomla\CMS\Factory;
+
+defined('_JEXEC') or die;
+
+$params = Factory::getApplication()->getParams();
+
+if(is_null($location)){
+    return;
+}
+?>
+
+<li class="partner" data-group="<?php echo implode(" ", $location->filters);?>">
+	<div class="uk-grid uk-grid-small uk-flex uk-flex-middle">
+		<div class="uk-width-expand">
+			<?php echo $location->title;?>
+		</div>
+		<?php if($canSeeContactInfo):?>
+			<div class="nxd-contact-link-container">
+				<?php if($location->phone):?>
+					<a href="tel:<?php echo htmlspecialchars($location->phone);?>">
+						<span uk-icon="icon: receiver; ratio: 1.1"></span>
+					</a>
+				<?php endif;?>
+			</div>
+			<div class="nxd-contact-link-container">
+				<?php if($location->email):?>
+					<a href="mailto:<?php echo htmlspecialchars($location->email);?>">
+						<span uk-icon="icon: mail; ratio: 1.1"></span>
+					</a>
+				<?php endif;?>
+			</div>
+			<div class="nxd-contact-link-container">
+				<?php if($location->web):?>
+					<a href="<?php echo htmlspecialchars($location->web);?>" target="_blank">
+						<span uk-icon="icon: world; ratio: 1.1"></span>
+					</a>
+				<?php endif;?>
+			</div>
+		<?php endif;?>
+	</div>
+</li>
---
+++ components/com_footballmanager/tmpl/teams/default.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_footballmanager
+ *
+ * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use Joomla\CMS\Factory;
+
+defined('_JEXEC') or die;
+
+$params = Factory::getApplication()->getParams();
+
+$wa = $this->document->getWebAssetManager();
+if(intval($params->get('load_uikit','1'))){
+    $wa->useScript('com_footballmanager.uikitjs')
+        ->useScript('com_footballmanager.uikitIconsjs')
+        ->useStyle('com_footballmanager.uikitcss');
+}
+
+$containerThemeCls = "";
+if($params->get('theme','light') === 'dark'){
+	$containerThemeCls = " uk-light";
+}
+
+$wa->useStyle('com_footballmanager.main');
+
+//// Limit Contact Information to defined groups if active:
+//$canSeeContactInfo = false;
+//$allowedUserGroups = $params->get('limit_contactoptions', array());
+//if(!empty($allowedUserGroups)){
+//    $user = Factory::getUser();
+//    $thisUsersGroups = array_intersect($allowedUserGroups, $user->getAuthorisedViewLevels());
+//    if(!empty($thisUsersGroups)){
+//        $canSeeContactInfo = true;
+//    }
+//}else{
+//    $canSeeContactInfo = true;
+//}
+//
+//$filterCls = "";
+//switch($params->get('filter_type', 'tabs')){
+//    case 'subnav':
+//        $filterCls = "uk-subnav";
+//        break;
+//    case 'subnav-pills':
+//        $filterCls = "uk-subnav uk-subnav-pill";
+//        break;
+//    case 'tabs':
+//    default:
+//        $filterCls = "uk-tab";
+//}
+//
+//$uikitListCls = 'uk-list-' . $params->get('items_list_type', 'divider') . ' uk-list-' . $params->get('items_list_size', 'large');
+
+?>
+
+hello locations!
